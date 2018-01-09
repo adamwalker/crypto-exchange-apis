aa9e9d36fd5ea06ebcff09325752b8dfa8236ee4
Adam Walker <adamwalker10@gmail.com>
2018-01-09 21:50:50 +1100

    itbit basic functionality

--- itbit-api/app/itbit.hs
+++ itbit-api/app/itbit.hs
@@ -6,7 +6,6 @@ import Data.ByteString.Lazy (ByteString)
 import Data.Monoid
 import Data.Maybe
 import Control.Monad.Trans.Except
-import Control.Monad
 
 import Data.ByteString.Lazy.Char8 (pack)
 import qualified Data.ByteString.Char8 as BSS
@@ -17,6 +16,7 @@ import Data.Yaml (decodeEither', ParseException)
 import System.Directory
 import Data.Either.Combinators
 import System.Environment
+import Network.HTTP.Req hiding (header)
 
 import ItBit
 
@@ -73,17 +73,47 @@ getConfigEnv
     where
     hackEnv = Last . fmap pack
 
-doIt :: (PartialAPIKey, Int) -> ExceptT String IO ()
-doIt (cmdlineAPIKey, nonce) = do
+parseSide :: ReadM Side
+parseSide = eitherReader $ \arg -> case arg of
+    "Buy"  -> return Buy
+    "Sell" -> return Sell
+    _      -> Left $ "Cannot parse side"
+
+parseOrder :: Parser Order
+parseOrder 
+    =   Order
+    <$> argument parseSide       (metavar "SIDE")
+    <*> pure XBT
+    <*> argument auto            (metavar "AMOUNT")
+    <*> argument auto            (metavar "PRICE")
+    <*> pure XBTUSD
+
+data Command
+    = OrderCmd   ByteString Order
+    | BalanceCmd 
+
+parseCommand :: Parser Command
+parseCommand = hsubparser $ mconcat [
+        command "Order"   (info (OrderCmd <$> (argument auto (metavar "WALLET_ID")) <*> parseOrder) (progDesc "Create an order")),
+        command "Balance" (info (pure BalanceCmd)                                                   (progDesc "Get balances"))
+    ]
+
+doIt :: (PartialAPIKey, Int, Command) -> ExceptT String IO ()
+doIt (cmdlineAPIKey, nonce, command) = do
     fileAPIKey <- getConfigFile
     envAPIKey  <- liftIO getConfigEnv
     apiKey     <- ExceptT $ return $ apiKeyFromMaybe $ fromMaybe mempty fileAPIKey <> envAPIKey <> cmdlineAPIKey
-    void $ liftIO $ doRequest apiKey nonce
+
+    res <- case command of
+        OrderCmd   walletID order -> liftIO $ doRequestPost apiKey nonce (newOrderPath <> walletID <> "/orders") (toJSON order)
+        BalanceCmd                -> liftIO $ doRequest     apiKey nonce 
+
+    liftIO $ print $ responseBody res
 
 main :: IO ()
 main = execParser opts >>= runExceptT . doIt >>= printErr
     where
-    opts = info (helper <*> ((,) <$> parseAPIKeys <*> parseNonce)) (fullDesc <> header "ItBit API")
+    opts = info (helper <*> ((,,) <$> parseAPIKeys <*> parseNonce <*> parseCommand)) (fullDesc <> header "ItBit API")
 
     parseNonce = argument auto (metavar "NONCE")
 
--- itbit-api/src/ItBit.hs
+++ itbit-api/src/ItBit.hs
@@ -6,6 +6,7 @@ import Data.ByteString.Lazy (ByteString)
 import Data.Monoid
 import Data.Default.Class
 import Data.Maybe
+import Text.Printf
 
 import Data.ByteString.Lazy.Char8 (pack)
 import qualified Data.ByteString.Base64.Lazy as B64
@@ -34,12 +35,42 @@ sign secretKey url nonce ts = result
     result :: ByteString
     result = B64.encode hmaced
 
+escape :: String -> String
+escape = concatMap func 
+    where
+    func '"' = "\\\""
+    func x   = [x]
+
+signPost :: ByteString -> ByteString -> ByteString -> ByteString -> ByteString -> (ByteString, ByteString)
+signPost secretKey url body nonce ts = (toHash, result)
+    where
+    toHash :: ByteString
+    toHash = nonce <> "[\"POST\",\"" <> url <> "\",\"" <> BS.pack (escape (BS.unpack body)) <> "\",\"" <> nonce <> "\",\"" <> ts <> "\"]"
+
+    hashed :: ByteString
+    hashed = bytestringDigest $ sha256 toHash
+
+    prepended :: ByteString
+    prepended = url <> hashed
+
+    hmaced :: ByteString
+    hmaced = bytestringDigest $ hmacSha512 secretKey prepended
+
+    result :: ByteString
+    result = B64.encode hmaced
+
 data Currency
     = USD
     | XBT
     | EUR
     | SGD
     deriving (Show, Eq)
+
+instance ToJSON Currency where
+    toJSON USD = "USD"
+    toJSON XBT = "XBT"
+    toJSON EUR = "EUR"
+    toJSON SGD = "SGD"
 
 instance FromJSON Currency where
     parseJSON = withText "Currency" $ \case
@@ -82,17 +113,50 @@ data Instrument
 instance ToJSON Instrument where
     toJSON XBTUSD = String "XBTUSD"
 
---data Order = Order {
---    side       :: Side,
---    currency   :: Currency,
---    amount     :: Double,
---    price      :: Double,
---    instrument :: Instrument
---} deriving (Show)
+data Side 
+    = Buy
+    | Sell
+    deriving (Show)
 
-apiRoot, walletsPath :: ByteString
+instance ToJSON Side where
+    toJSON Buy = String "buy"
+    toJSON Sell = String "sell"
+
+data Order = Order {
+    side       :: Side,
+    currency   :: Currency,
+    amount     :: Double,
+    price      :: Double,
+    instrument :: Instrument
+} deriving (Show)
+
+instance ToJSON Order where
+    toJSON Order{..} = object [
+            "side"       .= side,
+            "type"       .= ("limit" :: String),
+            "currency"   .= currency,
+            "amount"     .= (printf "%.4f" amount :: String),
+            "display"    .= (printf "%.4f" amount :: String),
+            "price"      .= (printf "%.2f" price :: String),
+            "instrument" .= instrument
+        ]
+    toEncoding Order{..} = pairs $
+           "side"       .= side
+        <> "type"       .= ("limit" :: String)
+        <> "currency"   .= currency
+        <> "amount"     .= (printf "%.4f" amount :: String)
+        <> "display"    .= (printf "%.4f" amount :: String)
+        <> "price"      .= (printf "%.2f" price  :: String)
+        <> "instrument" .= instrument
+
+apiRoot :: ByteString
 apiRoot = "https://api.itbit.com"
+
+walletsPath :: ByteString
 walletsPath = "/v1/wallets"
+
+newOrderPath :: ByteString
+newOrderPath = "/v1/wallets/"
 
 data APIKey = APIKey {
     userId     :: ByteString,
@@ -140,4 +204,36 @@ doRequest APIKey{..} nonce' = runReq def $ do
     --    parsed = fromJSON $ responseBody r
 
     --liftIO $ print parsed
+
+doRequestPost :: APIKey -> Int -> ByteString -> Value -> IO (JsonResponse Value)
+doRequestPost APIKey{..} nonce' path payload = runReq def $ do
+
+    currentTime <- liftIO $ getPOSIXTime
+
+    let nonce = pack $ show nonce'
+
+    let (toSign, signature) = signPost
+            privateKey 
+            (apiRoot <> path)
+            (encode payload)
+            nonce
+            (pack (show (floor (currentTime * 1000))))
+
+    liftIO $ BS.putStrLn toSign
+
+    r <- req 
+        POST -- method
+        (fst $ fromJust $ parseUrlHttps $ BS.toStrict $ apiRoot <> path)
+        (ReqBodyJson payload)
+        jsonResponse -- specify how to interpret response
+        (
+               header "Authorization"    (BS.toStrict publicKey <> ":" <> BS.toStrict signature)
+            <> header "X-Auth-Timestamp" (BSS.pack (show (floor (currentTime * 1000))))
+            <> header "X-Auth-Nonce"     (BS.toStrict nonce)
+            <> header "Content-Type"     "application/json"
+        )
+
+    liftIO $ print $ responseBody r
+
+    return r
 
